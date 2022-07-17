510e75cb91f891f632c5d8266f2e3839445b20a4
taggertt <taggertt@mail.ru>
2022-07-17 11:33:23 +0500

    rework code, add name

--- src/main/java/ru/netology/sqr/MavenSqr/services/SQRService.java
+++ src/main/java/ru/netology/sqr/MavenSqr/services/SQRService.java
@@ -3,15 +3,15 @@ package ru.netology.sqr.MavenSqr.services;
 public class SQRService {
 
 
-    public int rangeLimit(int a, int b) {
+    public int rangeLimit(int underLimit, int upperLimit) {
 
-        int c = 0; //Результат расчёта метода
+        int countSolutions = 0; //Результат расчёта метода
 
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= a && i * i <= b) {
-                c = c + 1;
+            if (i * i >= underLimit && i * i <= upperLimit) {
+                countSolutions = countSolutions + 1;
             }
         }
-        return c;
+        return countSolutions;
     }
 }
